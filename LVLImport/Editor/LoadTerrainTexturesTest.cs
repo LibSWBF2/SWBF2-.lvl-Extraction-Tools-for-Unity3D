497dbb572d473640c1d3ad045e557da6975ef502
William Herald Snyder <will.herald.snyder@gmail.com>
2020-08-25 11:18:43 -0400

    Terrain importing, but incorrectly, though results are not incohorent

--- LVLImport/Editor/LoadTerrainTexturesTest.cs
+++ LVLImport/Editor/LoadTerrainTexturesTest.cs
@@ -15,14 +15,14 @@ public class lvlImportMenu : ScriptableObject {
     [MenuItem("SWBF2/Import Terrain", false, 1)]
     public static void ImportTerrain() {
 
-		LibSWBF2.Logging.Logger.SetLogLevel(ELogType.Warning);
+        LibSWBF2.Logging.Logger.SetLogLevel(ELogType.Warning);
         LibSWBF2.Logging.Logger.OnLog += (LoggerEntry logEntry) => 
         {
             Debug.Log(logEntry.ToString());
         };
 
         Debug.Log("Loading... This might take a while...");
-        Level level = Level.FromFile(@"/Users/will/Desktop/omni.lvl");
+        Level level = Level.FromFile(@"/Users/will/Desktop/geo1.lvl");
         LibSWBF2.Wrappers.Terrain terrain = level.GetTerrain();
 
         foreach (var str in terrain.Names)
@@ -39,7 +39,7 @@ public class lvlImportMenu : ScriptableObject {
                 Color[] colors = tex.GetPixels(0);
                 for (int i = 0; i < height * width; i++)
                 {
-                	colors[i] = new Color(data[i*4]/255.0f,data[i*4 + 1]/255.0f,data[i*4 + 2]/255.0f,data[i*4 + 3]/255.0f);
+                    colors[i] = new Color(data[i*4]/255.0f,data[i*4 + 1]/255.0f,data[i*4 + 2]/255.0f,data[i*4 + 3]/255.0f);
                 }
                 tex.SetPixels(colors,0);
                 tex.Apply();
@@ -54,18 +54,33 @@ public class lvlImportMenu : ScriptableObject {
             Debug.Log(printStr);
         }
 
-        /*
-        TerrainData data = new TerrainData();
-        data.heightmapResolution = 16 * 9;
+        
+        TerrainData terData = new TerrainData();
+        terData.heightmapResolution = 256 + 1;
+        terData.size = new Vector3(16 * 9, 15, 16 * 9);
 
-		data.baseMapResolution = 1024;
-        data.SetDetailResolution(1024, 8);
+        terData.baseMapResolution = 1024;
+        terData.SetDetailResolution(1024, 8);
 
 
-        float[] heights = new float[256 * 81];
-		*/
+        float[] heightsRaw = terrain.Heights;
+
+        Debug.Log("Length of heights = " + heightsRaw.Length);
+
+        float[,] heights = new float[16*9,16*9];
+        for (int x = 0; x < 16 * 9; x++){
+            for (int y = 0; y < 16 * 9; y++){
+                heights[x,y] = heightsRaw[x * 16 * 9 + y];
+            }
+        }
+
+        terData.SetHeights(0, 0, heights);
+
+        GameObject terrainObj = UnityEngine.Terrain.CreateTerrainGameObject(terData);
+        PrefabUtility.SaveAsPrefabAsset(terrainObj, Application.dataPath + "/Terrain/terrain.prefab");
+        
+        AssetDatabase.Refresh();
 
         Debug.Log("Done");
     }
 }
-
