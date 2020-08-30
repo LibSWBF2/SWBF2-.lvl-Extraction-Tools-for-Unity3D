e55cfa3c7c406b05b55f5335b7b0bdfe766634c7
Will Snyder <will.herald.snyder@gmail.com>
2020-08-29 20:04:03 -0400

    More texture loading functions

--- LVLImport/Editor/LoadTexturesTest.cs
+++ LVLImport/Editor/LoadTexturesTest.cs
@@ -12,53 +12,35 @@ using LibSWBF2.Wrappers;
 
 public class TextureLoader : ScriptableObject {
 
+    public static Dictionary<string, Texture2D> texDataBase = new Dictionary<string, Texture2D>();
+    public static bool imported = false;
+
     public static Texture2D ImportTexture(Level level, string name) {
 
+        string texturePath = Application.dataPath + "/Textures/" + Regex.Replace(name, @"\s+", "") + ".png";
 
+        if (File.Exists(texturePath) && reuse)
+        {
 
-            if (level.GetTexture(str, out byte[] data, out int width, out int height))
-            {
-                Texture2D tex = new Texture2D(width,height);
-                Color[] colors = tex.GetPixels(0);
-                for (int i = 0; i < height * width; i++)
-                {
-                    colors[i] = new Color(data[i*4]/255.0f,data[i*4 + 1]/255.0f,data[i*4 + 2]/255.0f,data[i*4 + 3]/255.0f);
-                }
-                tex.SetPixels(colors,0);
-                tex.Apply();
-                byte[] pngBytes = tex.EncodeToPNG();
-                File.WriteAllBytes(Application.dataPath + "/Textures/" + Regex.Replace(str, @"\s+", "") + ".png", pngBytes);
-            }
-            else 
-            {
-                Debug.Log("Terrain Import Failed!");
-                return;
-            }
         }
 
-        float[] heightsRaw = terrain.Heights;
-        int dim = terrain.width;
-        
-        TerrainData terData = new TerrainData();
-        terData.heightmapResolution = terrain.width + 1;
-        terData.size = new Vector3(terrain.width, 15, dim);
-        terData.baseMapResolution = 1024;
-        terData.SetDetailResolution(1024, 8);
-
-        float[,] heights = new float[terrain.width,terrain.height];
-        for (int x = 0; x < terrain.width; x++){
-            for (int y = 0; y < terrain.height; y++){
-                heights[x,y] = heightsRaw[x * terrain.width + y];
+        if (level.GetTexture(str, out byte[] data, out int width, out int height))
+        {
+            Texture2D tex = new Texture2D(width,height);
+            Color[] colors = tex.GetPixels(0);
+            for (int i = 0; i < height * width; i++)
+            {
+                colors[i] = new Color(data[i*4]/255.0f,data[i*4 + 1]/255.0f,data[i*4 + 2]/255.0f,data[i*4 + 3]/255.0f);
             }
+            tex.SetPixels(colors,0);
+            tex.Apply();
+            File.WriteAllBytes(texturePath, tex.EncodeToPNG());
         }
-
-        terData.SetHeights(0, 0, heights);
-
-        GameObject terrainObj = UnityEngine.Terrain.CreateTerrainGameObject(terData);
-        PrefabUtility.SaveAsPrefabAsset(terrainObj, Application.dataPath + "/Terrain/terrain.prefab");
+        else 
+        {
+            Debug.Log("Texture Import Failed!");
+            return null;
+        }
         
-        AssetDatabase.Refresh();
-
-        Debug.Log("Terrain Imported!");
     }
 }
