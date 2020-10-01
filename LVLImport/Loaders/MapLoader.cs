88ff10adba2ab8667d8f5e2103b6b145f68c0b23
Will Snyder <will.herald.snyder@gmail.com>
2020-10-01 18:33:28 -0400

    Transform corrections for lights and objects differentiated and correct.

--- LVLImport/Loaders/MapLoader.cs
+++ LVLImport/Loaders/MapLoader.cs
@@ -45,9 +45,12 @@ public class MapLoader : ScriptableObject {
 
                     if (newObj != null)
                     {
-                        newObj.transform.localScale = new UnityEngine.Vector3(1.0f,1.0f,-1.0f);
+                        Debug.Log("Object: " + newObj.name + "\n\tRotation: " + UnityUtils.QuatFromLib(inst.GetRotation()).ToString() + "\n\tLocation: " + UnityUtils.Vec3FromLib(inst.GetPosition()).ToString() );
+
+                        newObj.transform.localScale = new UnityEngine.Vector3(-1.0f,1.0f,1.0f);
                         newObj.transform.rotation = UnityUtils.QuatFromLib(inst.GetRotation());
                         newObj.transform.position = UnityUtils.Vec3FromLib(inst.GetPosition());
+                        newObj.transform.parent = worldRoot.transform;
                     }
                 }
                 else 
@@ -64,16 +67,23 @@ public class MapLoader : ScriptableObject {
         Lighting -- Still don't know why Z coord has to be reversed + Y coord slightly increased...
         */
 
+        Debug.Log("=============================================================");
+
         foreach (var light in level.GetLights()) 
         {
             GameObject lightObj = new GameObject();
-            lightObj.transform.rotation = UnityUtils.QuatFromLib(light.rotation);
+
+            lightObj.transform.rotation = UnityUtils.QuatFromLibLGT(light.rotation);
+
+            light.position.Z *= -1.0f;
             lightObj.transform.position = UnityUtils.Vec3FromLib(light.position);
+
             lightObj.name = light.name;
+
 
             UnityEngine.Light lightComp = lightObj.AddComponent<UnityEngine.Light>();
             lightComp.color = UnityUtils.ColorFromLib(light.color);
-            lightComp.intensity = 3;
+            lightComp.intensity = 1;
 
             LibSWBF2.Enums.LightType ltype = light.lightType;
 
@@ -92,7 +102,7 @@ public class MapLoader : ScriptableObject {
             else if (ltype == LibSWBF2.Enums.LightType.Dir)
             {
                 lightComp.type = UnityEngine.LightType.Directional;
-                lightComp.intensity = 3;
+                lightComp.intensity = 1;
                 //lightComp.range = light.range;
                 //lightComp.spotAngle = light.spotAngles.X * Mathf.Rad2Deg;   
             }
