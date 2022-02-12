f70a428a87b2ee2273382b674a7f3c1c20941de6
William Herald Snyder <will.herald.snyder@gmail.com>
2022-02-12 16:08:02 -0500

    SoundLoader refactor to singleton + support for basic Config indirection.  Config indirection must be manually initialized on a per-level basis when each is sucessfully loaded.

--- LVLImport/Loaders/SoundLoader.cs
+++ LVLImport/Loaders/SoundLoader.cs
@@ -1,33 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+
 using LibSWBF2.Wrappers;
+using LibSWBF2.Enums;
+using LibSWBF2.Utils;
 
 public class SoundLoader : Loader
 {
-    static Dictionary<string, AudioClip> SoundDB = new Dictionary<string, AudioClip>();
+    public static SoundLoader Instance { get; private set; } = null;
 
-    public static void ResetDB()
+
+    Dictionary<uint, AudioClip> SoundDB = new Dictionary<uint, AudioClip>();
+    Dictionary<uint, uint> NameHashToClipMapping = new Dictionary<uint, uint>();
+
+
+    static SoundLoader()
     {
+        Instance = new SoundLoader();
+    }
+
+
+    public void ResetDB()
+    {
+        // For now, we don't clear the nametohash mapping when Resetting
         SoundDB.Clear();
     }
 
-    public static AudioClip LoadSound(string soundName)
+
+    public void InitializeSoundProperties(Level level)
     {
-        if (SoundDB.TryGetValue(soundName, out AudioClip foundClip))
+        if (level == null || !level.IsValid())
+        {
+            return;
+        }
+
+        foreach (Config sndCfg in level.GetConfigs(EConfigType.Sound))
+        {
+            foreach (Field soundField in sndCfg.GetFields("SoundProperties"))
+            {
+                uint name = soundField.Scope.GetUInt("Name");
+
+                Field pitch = soundField.Scope.GetField("Pitch");
+
+                Field sampleList;
+                try {
+                    sampleList = soundField.Scope.GetField("SampleList");
+                }
+                catch
+                {
+                    continue;
+                }
+                if (sampleList == null)
+                {
+                    continue;
+                }
+
+                Field[] samples = sampleList.Scope.GetFields("Sample");
+                if (samples.Length > 0)
+                {
+                    NameHashToClipMapping[name] = samples[0].GetUInt(0);
+                }
+            }
+        }
+    }
+
+
+
+    public AudioClip LoadSound(string soundName)
+    {
+        uint nameHash = HashUtils.GetFNV(soundName);
+        uint clipNameHash;
+        AudioClip foundClip;
+
+
+        if (NameHashToClipMapping.TryGetValue(HashUtils.GetFNV(soundName), out clipNameHash))
+        {
+            if (SoundDB.TryGetValue(clipNameHash, out foundClip))
+            {
+                return foundClip;
+            }
+        }
+        else if (SoundDB.TryGetValue(nameHash, out foundClip))
         {
             return foundClip;
         }
+        else
+        {
+            //Debug.LogWarningFormat("No sound mapping exists for {0}, attempting to get clip by name...", soundName);
+            clipNameHash = nameHash;
+        }
 
-        Sound sound = container.Get<Sound>(soundName);
+        Sound sound = container.Get<Sound>(clipNameHash);
         if (sound == null)
         {
+            Debug.LogWarningFormat("failed to find sound queried with: {0} (hash key: 0x{1:X})", soundName, clipNameHash);
             return null;
         }
 
         if (!sound.GetData(out uint sampleRate, out uint sampleCount, out byte blockAlign, out byte[] data))
         {
-            Debug.LogWarningFormat("Couldn't retrdieve sound data of sound '{0}'!", soundName);
+            Debug.LogWarningFormat("Couldn't retrieve sound data of sound '{0}'! (hash key: 0x{1:X})", soundName, clipNameHash);
             return null;
         }
         
@@ -44,10 +117,11 @@ public class SoundLoader : Loader
         
         if (!clip.SetData(pcm, 0))
         {
-            Debug.LogErrorFormat("Couldn't set sound data of sound '{0}'!", soundName);
+            Debug.LogErrorFormat("Couldn't set sound data of sound '{0}'! (hash key: 0x{1:X})", soundName, clipNameHash);
         }
 
-        SoundDB.Add(soundName, clip);
+        SoundDB.Add(clipNameHash, clip);
+
         return clip;
     }
 }
